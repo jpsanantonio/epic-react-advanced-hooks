76aeda3f735383025719cd2e70fbe4601e94edc8
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2023-08-08 18:39:36 -0700

    useContext: simple Counter - Create a Consumer Hook

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -5,18 +5,28 @@ import * as React from 'react';
 
 const CountContext = React.createContext();
 
+function useCount() {
+  const context = React.useContext(CountContext);
+
+  if (!context) {
+    throw new Error(`useCount may only be used from within the CountProvider`);
+  }
+
+  return context;
+}
+
 function CountProvider(props) {
   const value = React.useState(0);
   return <CountContext.Provider value={value} {...props} />;
 }
 
 function CountDisplay() {
-  const [count] = React.useContext(CountContext);
+  const [count] = useCount();
   return <div>{`The current count is ${count}`}</div>;
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext);
+  const [, setCount] = useCount();
   const increment = () => setCount(c => c + 1);
   return <button onClick={increment}>Increment count</button>;
 }
